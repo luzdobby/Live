0665917ba820c35500168a03551ccf08357bc64a
luzdobby <luzmariamontano274@gmai.com>
2024-10-19 23:03:59 -0600

    la ruta fue corregida

--- Html/login.php
+++ Html/login.php
@@ -4,39 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/Pagina Promocional/CSS/StyleLogin">
+    <link rel="stylesheet" href="/Livestok/CSS/StyleLogin.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
 
 <?php
-
-
-
-if(isset($_POST['btnInsertar'])){
-/*if(empty($_POST['Usuario']) || empty( $_POST['Clave'])){*/
- 
-
-   header('location:redi.php');
-
-
-
-    
-    
-    
-       }
-    else{
-
-
-
-    }
-    
+include '/xampp/htdocs/Livestok/PHP/Insertar.php';
 
 ?>
 
-
     <div class="Container">
-        <form action="" method="post" >
+        <form action="" method="post"  class="Formulario">
             <h1>Login</h1>
             <div class="input-box">
                 <input type="text" placeholder="User"  name="Usuario">
